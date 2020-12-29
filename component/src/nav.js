dbccd75290fee4ae69e1742853892697041599a9
freeman-lab <the.freeman.lab@gmail.com>
2020-12-29 14:54:46 -0800

    finish nav example

--- component/src/nav.js
+++ component/src/nav.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { default as NextLink } from 'next/link'
-import { Link } from 'theme-ui'
+import { Box, Link } from 'theme-ui'
 
 const Nav = () => {
   
-  return <NextLink href='/' passHref>
-    <Link>Home</Link>
-  </NextLink>
+  return <Box>
+    <Box sx={{fontSize: [5]}}>
+      This is a Next link that should navigate to /
+    </Box>
+    <NextLink href='/' passHref>
+      <Link sx={{fontSize: [5]}}>Home</Link>
+    </NextLink>
+  </Box>
 }
 
 export default Nav
